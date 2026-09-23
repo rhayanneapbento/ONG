8051da9a335bdbe220ef298f3fcef58a32d9ebcc
rhayanneapbento <rhayanneapbento@gmail.com>
2026-09-23 14:27:18 -0300

    Update storage.js

--- js/js/storage.js
+++ js/js/storage.js
@@ -1,1 +1,13 @@
+export function salvarFormulario(dadosFormulario) {
+    localStorage.setItem("dadosFormulario", JSON.stringify(dadosFormulario));
+}
 
+export function carregarFormulario() {
+    const dadosSalvos = localStorage.getItem("dadosFormulario");
+
+    if (dadosSalvos) {
+        return JSON.parse(dadosSalvos);
+    }
+
+    return null;
+}
